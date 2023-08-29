cb13292fe050f46fddaf0f77d7925548b9b464ad
Aidar <aydararzybekov2002@mail.ru>
2023-08-29 19:46:11 +0600

    create button config

--- src/pages/main/tsx/MainPage.tsx
+++ src/pages/main/tsx/MainPage.tsx
@@ -1,7 +1,65 @@
+import { useState } from 'react'
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/button/Button'
 import cls from './MainPage.module.scss'
 
 const Main = () => {
-	return <div className={cls.main}>Main Page</div>
+	const [dis, setDis] = useState(false)
+	const clickButt = () => {
+		setDis(!dis)
+	}
+
+	return (
+		<div className={cls.main}>
+			<Button
+				theme={ButtonTheme.BACKGROUND}
+				size={ButtonSize.S}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Primary
+			</Button>
+			<Button
+				theme={ButtonTheme.BACKGROUND_ORANGE}
+				size={ButtonSize.M}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Orange
+			</Button>
+			<Button
+				theme={ButtonTheme.BACKGROUND}
+				size={ButtonSize.L}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Orange
+			</Button>
+			<Button
+				theme={ButtonTheme.OUTLINE_ORANGE}
+				size={ButtonSize.L}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Orange
+			</Button>
+			<Button
+				theme={ButtonTheme.OUTLINE}
+				size={ButtonSize.M}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Orange
+			</Button>
+			<Button
+				theme={ButtonTheme.OUTLINE}
+				size={ButtonSize.S}
+				disabled={dis}
+				onClick={clickButt}
+			>
+				Orange
+			</Button>
+		</div>
+	)
 }
 
 export default Main
---
+++ src/shared/ui/button/Button.tsx
@@ -0,0 +1,52 @@
+import { classNames, Mods } from 'shared/lib/classNames/classNames'
+import { ButtonHTMLAttributes, FC, memo, ReactNode } from 'react'
+import cls from './Button.module.scss'
+
+export enum ButtonTheme {
+	OUTLINE = 'outline',
+	OUTLINE_ORANGE = 'outlineOrange',
+	BACKGROUND = 'background',
+	BACKGROUND_ORANGE = 'backgroundOrange',
+}
+
+export enum ButtonSize {
+	S = 'size_s',
+	M = 'size_m',
+	L = 'size_l',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string
+	theme?: ButtonTheme
+	size?: ButtonSize
+	disabled?: boolean
+	children?: ReactNode
+}
+
+export const Button = memo((props: ButtonProps) => {
+	const {
+		className,
+		children,
+		theme = ButtonTheme.OUTLINE,
+		disabled,
+		size = ButtonSize.M,
+		...otherProps
+	} = props
+
+	const mods: Mods = {
+		[cls[theme]]: true,
+		[cls[size]]: true,
+		[cls.disabled]: disabled,
+	}
+
+	return (
+		<button
+			type='button'
+			className={classNames(cls.Button, mods, [className])}
+			disabled={disabled}
+			{...otherProps}
+		>
+			{children}
+		</button>
+	)
+})
