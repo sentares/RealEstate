243878c2803fa1a879f2d04a90372c744599af9d
Aidar <aydararzybekov2002@mail.ru>
2023-09-21 15:30:41 +0600

    create select

--- src/pages/main/tsx/MainPage.tsx
+++ src/pages/main/tsx/MainPage.tsx
@@ -3,16 +3,60 @@ import { useSelector } from 'react-redux'
 import { ThemeSwitcher } from 'shared/switchers/theme-switcher/ThemeSwitcher'
 import cls from './MainPage.module.scss'
 import { DefCard } from 'features/RealtyCard'
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/button/Button'
+import Switcher, { SwitcherTheme } from 'shared/ui/switcher/Switcher'
+import { useState } from 'react'
+import { MapPin } from 'lucide-react'
+import Select from 'shared/ui/select/Select'
+
+interface Option {
+	id: number | null
+	name: string
+}
 
 const Main = () => {
 	const data = useSelector(getRealty)
 
+	const optionsCategory = [
+		{ id: 1, name: 'Мебель' },
+		{
+			id: 2,
+			name: 'Оборудование',
+		},
+		{
+			id: 3,
+			name: 'Вентиляция',
+		},
+	]
+
+	const [clickedItem, setClickedItem] = useState<Option>({
+		id: null,
+		name: '',
+	})
+
+	const handleClickeItem = (item: Option) => {
+		setClickedItem(item)
+	}
+
+	const handleClickRemoveItem = () => {
+		setClickedItem({ id: null, name: '' })
+	}
+
 	return (
 		<div className={cls.main}>
+			<Button className={cls.butt}>hello</Button>
 			<ThemeSwitcher className={cls.themeSwitch} />
 			<div>
 				<DefCard />
 			</div>
+			<Select
+				placeholder='All types'
+				className={cls.select}
+				options={optionsCategory}
+				handleClickeItem={handleClickeItem}
+				handleClickRemoveItem={handleClickRemoveItem}
+				clickedItem={clickedItem}
+			/>
 		</div>
 	)
 }
---
+++ src/shared/ui/select/Select.tsx
@@ -0,0 +1,103 @@
+import { memo, useState } from 'react'
+import cls from './Select.module.scss'
+import { Mods, classNames } from 'shared/lib/classNames/classNames'
+import { ChevronDown, XCircle } from 'lucide-react'
+
+interface Option {
+	id: number | null
+	name: string
+}
+
+export enum SelectTheme {
+	DEFAULT = 'default',
+}
+
+interface SelectProps {
+	className?: string
+	options?: Option[]
+	disabled?: boolean
+	placeholder?: string
+	clickedItem?: Option
+	listStyles?: {}
+	theme?: SelectTheme
+	onClick?: (e: React.ChangeEvent<HTMLSelectElement>) => void
+	handleClickeItem?: (item: Option) => void
+	handleClickRemoveItem?: () => void
+}
+
+const Select = memo((props: SelectProps) => {
+	const [open, setOpen] = useState(false)
+	const handleDropdownClick = () => {
+		setOpen(!open)
+	}
+
+	const {
+		options,
+		className,
+		placeholder,
+		clickedItem,
+		listStyles,
+		disabled,
+		theme = SelectTheme.DEFAULT,
+		handleClickeItem,
+		handleClickRemoveItem,
+		...otherProps
+	} = props
+
+	const mods: Mods = {
+		[cls[theme]]: true,
+		[cls.disabled]: disabled,
+	}
+
+	return (
+		<div className={classNames(cls.Select, mods, [className])}>
+			<button
+				id='dropdownInformationButton'
+				onClick={handleDropdownClick}
+				className={`${cls['dropdown-button']} ${open && cls.iconOpen} `}
+				disabled={disabled}
+			>
+				{clickedItem?.name ? clickedItem.name : placeholder}
+				<ChevronDown className={`${cls.icon} ${cls.iconClosed}`} />
+			</button>
+			{open && (
+				<ul className={cls['dropdown-list']} style={listStyles}>
+					{options &&
+						options.map(item => (
+							<li key={item.id} className={cls['dropdown-block']}>
+								<div
+									onClick={
+										handleClickeItem && handleClickeItem.bind(null, item)
+									}
+									className={cls['dropdown-item']}
+									style={
+										item.id === clickedItem?.id
+											? {
+													background: '#006d39',
+													color: 'white',
+													fontWeight: 700,
+													boxShadow:
+														'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',
+											  }
+											: {}
+									}
+								>
+									{item.name}
+								</div>
+								{item.id === clickedItem?.id && (
+									<XCircle
+										width={18}
+										cursor='pointer'
+										className={cls.closeIcon}
+										onClick={handleClickRemoveItem}
+									/>
+								)}
+							</li>
+						))}
+				</ul>
+			)}
+		</div>
+	)
+})
+
+export default Select
